3f8b6f47458ca7753ebd9443f9fd0f8a7fab09cb
amitsoren1 <amt.sr.101@gmail.com>
2020-09-01 03:46:20 +0530

    modified put method of post

--- api/signals.py
+++ api/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save,pre_save
 from django.dispatch import receiver
 from .serializers import ProfileSerializer
 
@@ -15,16 +15,10 @@ def get_url(instance):
     url = "https://pubgapi.pythonanywhere.com/"+serializer.data.get("profile_pic")
     return url
 
-# @receiver(post_save, sender=Profile)
-# def update_or_create_avatar_link(sender, instance, created, **kwargs):
-#     url = get_url(instance)
-#     print(url)
-#     import requests
-#     pload = {'username':instance.user.username,'avatar':url}
-#     try:
-#         r = requests.post('http://127.0.0.1:5000/update-pic',data = pload)
-#         print(r)
-#     except:
-#         pass
-#     # print(r.text)
-#     # pass
+@receiver(pre_save, sender=Profile)
+def update_or_create_avatar_link(sender, instance, created, **kwargs):
+    print(instance.profile_pic.path)
+
+@receiver(post_save, sender=Profile)
+def update_or_create_avatar_link(sender, instance, created, **kwargs):
+    print(instance.profile_pic.path)
