77d6fb9eb032fe658acf85853b483f93484aa182
amitsoren1 <amt.sr.101@gmail.com>
2020-08-31 23:46:30 +0530

    changed cors list

--- blogback/settings.py
+++ blogback/settings.py
@@ -59,7 +59,10 @@ MIDDLEWARE = [
 #     'django.middleware.common.CommonMiddleware',
 # )
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = (
+    'pubgapi.pythonanywhere.com',
+    'youthful-snyder-b6a635.netlify.app'
+)
 
 ROOT_URLCONF = 'blogback.urls'
 
