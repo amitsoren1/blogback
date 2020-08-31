be07f36b8976ce29f0083d557bb3a24bb3b39529
amitsoren1 <amt.sr.101@gmail.com>
2020-09-01 04:50:01 +0530

    modified put method of post

--- api/signals.py
+++ api/signals.py
@@ -7,12 +7,16 @@ import os, pathlib
 
 FILE_PATH = ""
 
+def set_path(path):
+    global FILE_PATH
+    FILE_PATH = path
+
 def get_url(instance):
     file_name = instance.profile_pic.path.split("/")[-1]
-    url = "https://pubgapi.pythonanywhere.com/"+"media/pic_folder/"+f"{profile.user.username}/"+file_name
+    url = "https://pubgapi.pythonanywhere.com/"+"media/pic_folder/"+f"{instance.user.username}/"+file_name
     return url
 
 @receiver(post_save, sender=Profile)
 def after(sender, instance, created, **kwargs):
     global FILE_PATH
-    print(FILE_PATH)
+    os.remove(FILE_PATH)
